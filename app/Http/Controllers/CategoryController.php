852e443638918787af503605b8b29b0c03e0f127
marijano1515-oss <marijano1515@gmail.com>
2026-04-14 20:21:43 +0400

    update

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -32,7 +32,21 @@ class CategoryController extends Controller
 
     private function update(Request $request)
     {
-
+        if (!auth()->user() || auth()->user()->is_admin != 1)  {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 403);
+        }
+        $request->validate([
+            'name' => 'required|string|max:255|unique:categories',
+        ]);
+        $category = CategoryController::update([
+            'name' =>$request->name
+        ]);
+        return response()->json([
+            'category' => $category,
+            'message' => 'Category updated'
+        ]);
     }
 
     private function show(Request $request)
