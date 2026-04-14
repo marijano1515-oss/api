e62705cc905fec622104d6da5fa60bef93515d1b
marijano1515-oss <marijano1515@gmail.com>
2026-04-14 19:57:23 +0400

    Initial commit

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    private function index(Request $request)
+    {
+
+    }
+
+    private function store(Request $request,)
+    {
+        if (!auth()->user() || auth()->user()->is_admin != 1)  {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 403);
+        }
+        $request->validate([
+            'name' => 'required|string|max:255|unique:categories',
+        ]);
+        $category = CategoryController::create([
+            'name' => $request->name
+        ]);
+        return response()->json([
+            'category' => $category,
+            'message' => 'Category created'
+        ]);
+    }
+
+    private function update(Request $request)
+    {
+
+    }
+
+    private function show(Request $request)
+    {
+
+    }
+
+    private function delete(Request $request)
+    {
+
+    }
+}
---
+++ app/Models/Category.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+#[Fillable(['name'])]
+
+class Category extends Model
+{
+    public function casts()
+    {
+
+    }
+}
